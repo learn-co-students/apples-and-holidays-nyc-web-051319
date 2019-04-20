b316735fc7392b672cabc9487aa0fe6e0abca02c
Carla <carlastickler@gmail.com>
2019-04-20 15:35:12 -0400

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,4 +1,4 @@
-require 'pry'
+# require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
   # given that holiday_hash looks like this:
@@ -57,28 +57,35 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
+    # iterate through holiday_hash and print items such that your readout resembles:
+    # Winter:
+    #   Christmas: Lights, Wreath
+    #   New Years: Party Hats
+    # Summer:
+    #   Fourth Of July: Fireworks, BBQ
+    # etc.
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize! << ":"
     holiday.each do |holiday_name, supply|
       new_array = holiday_name.to_s.split("_").collect {|x| x.capitalize!}.join(" ")
       puts "  #{new_array}: #{supply.join(', ')}"
+    end
   end
-end
-  
-
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_w_BBQ = []
+  holiday_hash.each do |season, holiday|
+    
+    holiday.each do |holiday_name, supplies|
+      if supplies.include? ("BBQ")
+        holiday_w_BBQ << holiday_name
+      end
+    end
+  end
+  holiday_w_BBQ
 end
 
 
